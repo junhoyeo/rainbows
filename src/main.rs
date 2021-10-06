52962daad5ecc9f00614c448b5b4f60726201294
Junho Yeo <hanaro0704@gmail.com>
2021-10-06 12:23:53 +0900

    `--number-of-rainbows` option to manually change repeats

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,21 @@
+use clap::{AppSettings, Clap};
 use rand::prelude::*;
 
+#[derive(Clap)]
+#[clap(setting = AppSettings::ColoredHelp)]
+struct Opts {
+    #[clap(long, default_value = "0")]
+    number_of_rainbows: i32,
+}
+
 fn main() {
-    let random_int = rand::thread_rng().gen_range(1..1000);
-    let number_of_rainbows = random_int;
+    let opts: Opts = Opts::parse();
+    let number_of_rainbows =
+        if opts.number_of_rainbows == 0 {
+            rand::thread_rng().gen_range(1..1000)
+        } else {
+            opts.number_of_rainbows as usize
+        };
 
     let verb = if number_of_rainbows > 1 { "are" } else { "is" };
     let plurality = if number_of_rainbows > 1 { "s" } else { "" };
