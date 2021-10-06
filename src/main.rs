72214a2b10523ae0261d6118bc3c8c17ed9c9efa
Junho Yeo <hanaro0704@gmail.com>
2021-10-06 16:27:24 +0900

    Format with RLS

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,14 @@ fn main() {
         .about(env!("CARGO_PKG_DESCRIPTION"))
         .author(env!("CARGO_PKG_AUTHORS"))
         .version(env!("CARGO_PKG_VERSION"))
-        .arg(Arg::new("number-of-rainbows")
-            .short('n')
-            .long("number-of-rainbows")
-            .value_name("NUMBER")
-            .about("Manually sets the number of rainbows")
-            .default_value("0"))
+        .arg(
+            Arg::new("number-of-rainbows")
+                .short('n')
+                .long("number-of-rainbows")
+                .value_name("NUMBER")
+                .about("Manually sets the number of rainbows")
+                .default_value("0"),
+        )
         .get_matches();
 
     let given_number_of_rainbows: u16 = std::cmp::min(
@@ -22,12 +24,11 @@ fn main() {
             .unwrap_or(MAX_NUMBER_OF_RAINBOWS),
         MAX_NUMBER_OF_RAINBOWS,
     );
-    let number_of_rainbows =
-        if given_number_of_rainbows == 0 {
-            rand::thread_rng().gen_range(1..MAX_NUMBER_OF_RAINBOWS)
-        } else {
-            given_number_of_rainbows
-        } as usize;
+    let number_of_rainbows = if given_number_of_rainbows == 0 {
+        rand::thread_rng().gen_range(1..MAX_NUMBER_OF_RAINBOWS)
+    } else {
+        given_number_of_rainbows
+    } as usize;
 
     let verb = if number_of_rainbows > 1 { "are" } else { "is" };
     let plurality = if number_of_rainbows > 1 { "s" } else { "" };
