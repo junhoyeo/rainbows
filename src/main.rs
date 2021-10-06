6cd837a4641ba3dd014144f0098aede5f422c3c1
Junho Yeo <hanaro0704@gmail.com>
2021-10-06 11:31:17 +0900

    Print number of rainbows

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,13 @@ use rand::prelude::*;
 
 fn main() {
     let random_int = rand::thread_rng().gen_range(1..1000);
-    println!("{}", "🌈".repeat(random_int));
+    let number_of_rainbows = random_int;
+
+    let verb = if number_of_rainbows > 1 { "are" } else { "is" };
+    let plurality = if number_of_rainbows > 1 { "s" } else { "" };
+    println!(
+        "Here {} {} rainbow{} for you!",
+        verb, number_of_rainbows, plurality
+    );
+    println!("{}", "🌈".repeat(number_of_rainbows));
 }
